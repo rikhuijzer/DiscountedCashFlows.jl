2e0e7f70cb4859558b4fd8a28057abc41808a60d
rikhuijzer <rikhuijzer@pm.me>
2022-05-15 14:20:30 +0200

    Refactor and document

--- src/DiscountedCashFlows.jl
+++ src/DiscountedCashFlows.jl
@@ -1,6 +1,21 @@
 module DiscountedCashFlows
 
-export dcf, cash_flows
+export dcf
+
+"""
+    cash_flows(initial_cash_flow::Real, cash_flow_growth::Real, n::Int)
+
+Return a vector of `n` cash flows starting from the initial cash flow with an annual cash flow growth.
+"""
+function cash_flows(initial_cash_flow::Real, cash_flow_growth::Real, n::Int)
+    if cash_flow_growth < 1
+        error("""
+            Cash flow growth is assumed to be above 1.
+              For example, for an annual growth of 5%, pass 1.05.
+            """)
+    end
+    return [initial_cash_flow * cash_flow_growth^i for i in 1:n]
+end
 
 function dcf(cash_flows::AbstractVector{<:Real}, discount_rate::Real)
     out = 0.0
@@ -10,16 +25,23 @@ function dcf(cash_flows::AbstractVector{<:Real}, discount_rate::Real)
         out += discounted
     end
     return round(out; digits=1)
-end;
-
-function cash_flows(current_cash_flow::Real, cash_flow_growth::Real, nyears::Int)
-    @assert 1 < cash_flow_growth "Cash flow growth is assumed to be above 1. For example, for an annual growth of 5%, pass 1.05."
-    return [current_cash_flow * cash_flow_growth^i for i in 1:nyears]
 end
 
-function dcf(current_cash_flow::Real, cash_flow_growth::Real, nyears::Int, discount_rate::Real)
-    flows = cash_flows(current_cash_flow, cash_flow_growth, nyears)
+"""
+    dcf(initial_cash_flow::Real, cash_flow_growth::Real, n::Int, discount_rate::Real)
+
+Return the discounted cash flow.
+
+- `initial_cash_flow`: The cash flow in year 1.
+- `cash_flow_growth`: The estimated annual cash flow growth.
+    For a 10% annual growth, pass 1.10.
+- `n`: Number of years for which to calculate the annual cash flow.
+- `discount_rate`: The discount rate.
+    For a 10% annual discount rate, pass 0.10.
+"""
+function dcf(initial_cash_flow::Real, cash_flow_growth::Real, n::Int, discount_rate::Real)
+    flows = cash_flows(initial_cash_flow, cash_flow_growth, n)
     return dcf(flows, discount_rate)
-end;
+end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,13 +3,11 @@ using Test
 
 rint(x) = round(Int, x)
 
-const D = DiscountedCashFlows
-
 @test rint(dcf([4e5], 0.05)) == 380_952
 @test rint(dcf([4e5, 4e5], 0.05) - dcf([4e5], 0.05)) == 362_812
 @test rint(dcf([4e5, 4e5, 4e5, 4e5, 6e6], 0.05)) == 6_119_537
 
-@test cash_flows(10, 1.1, 2) == [10 * 1.1, 10 * 1.1^2]
+@test DiscountedCashFlows.cash_flows(10, 1.1, 2) == [10 * 1.1, 10 * 1.1^2]
 
 growth_rate = 1.07
 discount_rate = 0.10
